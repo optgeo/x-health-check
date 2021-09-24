4d62101c09c95b667b499bfeeb078f503df230f0
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2021-09-25 07:34:36 +0900

    update

--- health-check.rb
+++ health-check.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/env ruby
 require 'time'
-DST = "https://hooks.slack.com/services/----/----/-----------" # incoming webhook
+DST = "https://hooks.slack.com/services/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" # incoming webhook
 SRC = "https://x.optgeo.org/hrlulc/index.html"
 
-msg = `curl -o /dev/null -s -w %{http_code} #{SRC}`
-msg = "#{Time.now.iso8601} #{msg}"
+def pomocode
+  Time.now.to_i / 1800
+end
+
+msg = `curl -o /dev/null -s -w '%{http_code}\n' #{SRC}`
+msg = "[#{pomocode}] nginx status: #{msg}"
+
+# submit to Slack
 system <<-EOS
   curl -X POST -H 'Content-type: application/json' --data '{"text":"#{msg}"}' #{DST}
 EOS
